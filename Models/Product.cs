1f95a94814c08093cdd2165a482f98c218678558
Hamid reza <h.rostaminezhad@gmail.com>
2023-07-03 19:14:22 +0330

    add model Product complite Q2

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,6 +2,11 @@
 {
     public class Product
     {
-
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int PhotoUrl { get; set; }
+        public string Barcode { get; set; }
     }
 }
