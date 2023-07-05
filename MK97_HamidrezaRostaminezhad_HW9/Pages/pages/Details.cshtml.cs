264415b14ae1da1cd430f9ae77456eb952e8abb4
htrm95 <h.rostaminezhad1995@gmail.com>
2023-07-05 15:51:09 +0330

    chenge index product design

---
+++ MK97_HamidrezaRostaminezhad_HW9/Pages/pages/Details.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
+using Services.Interface;
+
+namespace MK97_HamidrezaRostaminezhad_HW9.Pages.pages
+{
+    public class DetailsModel : PageModel
+    {
+        private IProductRepository _productRepository;
+
+        public DetailsModel(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [BindProperty]
+        public Product product { get; set; }
+
+
+        public void OnGet(int id)
+        {
+            product = _productRepository.GetProductById(id);
+        }
+    }
+}
